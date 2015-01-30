5ddb82bfc23101cb4db36d36cddd500b36a0daeb
Eric Escobar <Eric@Escobar.com>
2015-01-30 11:53:19 -0800

    Finished input control portion

--- Input_Control.py
+++ Input_Control.py
@@ -3,32 +3,37 @@ import time
 import signal
 import sys
 
-print 'Door action: ',sys.argv[1]
-print 'Seconds: ',sys.argv[2]
-print 'Number of arguments: ',len(sys.argv)
+if len(sys.argv)>3: #Tests if you've entered too many arguments
+    print "You've entered too many arguments!"
+    print "Exiting program..."
+    sys.exit(0)
 
-if len(sys.argv)>3:
-    sys.exit()
-if len(sys.argv)==3:
+if len(sys.argv)>2: #Argument for door action time
     try:
         float(sys.argv[2])
     except:
         print 'Error: ',str(sys.argv[2]),' is not a number!'
+        print "Exiting program..."
+        sys.exit(0)
     if int(sys.argv[2])>45: #Checks that a time longer than 45s isn't entered
             print 'Please choose a time less than 45s'
+            print "Exiting program..."
             sys.exit(0)
-            
-if sys.argv[1]!='close':# or sys.argv[1]!='open':
-        print 'Please choose "open" or "close"'
-        sys.exit(0)
-if len(sys.argv)>3:
-        print 'Too many arguments!'
-        sys.exit(0)
-elif len(sys.argv)==2:
-        print 'Forcing door to ',str(sys.argv[1]),' for ',str(sys.argv[2]),' seconds'
-        Door_Action=sys.argv[1]
-        Door_Time=sys.argv[2]
-elif len(sys.argv)==1:
-        print 'Forcing door to ',str(sys.argv[1])
-        Door_Action=sys.argv[1]
-print 'Reached end!'
+
+if len(sys.argv)>1: #Argument for door action
+    if sys.argv[1]!='close' and sys.argv[1]!='open':
+            print 'Please choose "open" or "close"'
+            print "Exiting program..."
+            sys.exit(0)
+
+if len(sys.argv)==3:
+    print 'Forcing door to',str(sys.argv[1]),'for',str(sys.argv[2]),'seconds'
+    Door_Action=sys.argv[1]
+    Door_Time=sys.argv[2]
+if len(sys.argv)==2:
+    print 'Forcing door to ',str(sys.argv[1])
+    Door_Action=sys.argv[1]
+    Door_Time=45 #This is a safety time
+if len(sys.argv)==1:
+    Door_Action='default' #Will reverse door state
+    Door_Time=45 #This is a safety time
