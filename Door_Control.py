c5eac56e6bb69cfa0edbd50fa1457f4f734f1379
Eric Escobar <eric@escobar.com>
2015-02-02 21:55:24 -0800

    Door Control Finished
    Input control merged with door control after recovery.

--- Door_Control.py
+++ Door_Control.py
@@ -3,23 +3,7 @@ import RPi.GPIO as GPIO
 import time
 import signal
 import sys
-#Argument processing, open, close, open x, close x
-'''if int(sys.argv[2])>45:
-        print 'Please choose a shorter time'
-if sys.argv[1]!='close' or sys.argv[1]!='open':
-        print 'Please choose "open" or "close"'
-        sys.exit(0)
-if len(sys.argv)>3:
-        print 'Too many arguments!'
-        sys.exit(0)
-elif len(sys.argv)==2:
-        print 'Forcing door to ',str(sys.argv[1]),' for ',str(sys.argv[2]),' se$
-        Door_Action=sys.argv[1]
-        Door_Time=sys.argv[2]
-elif len(sys.argv)==1:
-        print 'Forcing door to ',str(sys.argv[1])
-        Door_Action=sys.argv[1]
-'''
+
 #Setting up Board GPIO Pins
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(35,GPIO.OUT)
@@ -28,13 +12,43 @@ GPIO.setup(33,GPIO.IN)#Locked
 GPIO.setup(31,GPIO.IN)#Open
 TimeStart=time.clock()
 runTime=0
-#Clean kill of script function (Stops Motor, cleans GPIO)
-def signal_handler(signal, frame):
-        print 'Performing safe shutdown!'
-        GPIO.output(37,False)
-        GPIO.output(35,False)
-        GPIO.cleanup()
-        sys.exit('Motors shutdown, GPIO cleaned')
+
+#Argument controller
+if len(sys.argv)>3: #Tests if you've entered too many arguments
+    print "You've entered too many arguments!"
+    print "Exiting program..."
+    sys.exit(0)
+
+if len(sys.argv)>2: #Argument for door action time
+    try:
+        float(sys.argv[2])
+    except:
+        print 'Error: ',str(sys.argv[2]),' is not a number!'
+        print "Exiting program..."
+        sys.exit(0)
+    if int(sys.argv[2])>45: #Checks that a time longer than 45s isn't entered
+            print 'Please choose a time less than 45s'
+            print "Exiting program..."
+            sys.exit(0)
+
+if len(sys.argv)>1: #Argument for door action
+    if sys.argv[1]!='stop' s.argv[1]!='open':    
+    if sys.argv[1]!='close' and sys.argv[1]!='open':
+            print 'Please choose "open" or "close"'
+            print "Exiting program..."
+            sys.exit(0)
+
+if len(sys.argv)==3:
+    print 'Forcing door to',str(sys.argv[1]),'for',str(sys.argv[2]),'seconds'
+    Door_Action=sys.argv[1]
+    Door_Time=sys.argv[2]
+if len(sys.argv)==2:
+    print 'Forcing door to ',str(sys.argv[1])
+    Door_Action=sys.argv[1]
+    Door_Time=45 #This is a safety time
+if len(sys.argv)==1:
+    Door_Action='default' #Will reverse door state
+    Door_Time=45 #This is a safety time
 
 #Check door status from Magnets
 BottomHall=GPIO.input(31)
@@ -44,25 +58,45 @@ if TopHall==0:print 'Door is open'
 if BottomHall==1:print 'No magnet sensed on lock'
 if TopHall==1:print 'No magnet sensed top'
 
-#if 0==0: #Debug Code
-if BottomHall==0: #Door is locked
-        print 'The door is locked!'
-        print 'The door is going up!'
-        while TopHall==1 and runTime<10:
-                GPIO.output(35,True)
-                GPIO.output(37,False)
-                TopHall=GPIO.input(33)
-                runTime=time.clock()-TimeStart
-#if 0==0:
-elif TopHall==0 and BottomHall==1: #Door is open
-        print 'The door is open!'
-        print 'The door is going down!'
-        while BottomHall==1 and runTime<35:
-                GPIO.output(35,False)
-                GPIO.output(37,True)
-                BottomHall=GPIO.input(31)
-                runTime=time.clock()-TimeStart
-runTime=time.clock()-TimeStart
-print 'Total Time: '+str(runTime)
-GPIO.cleanup()
+#Clean kill of script function (Stops Motor, cleans GPIO)
+def Safe_Kill():
+        print 'Performing safe shutoff!'
+        GPIO.output(37,False)
+        GPIO.output(35,False)
+        GPIO.cleanup()
+        sys.exit('Motors shutdown, GPIO cleaned')
+#Start door!
+def DoorControl():
+        if BottomHall==0 or Door_Action=='open': #Door is locked
+                print 'The door is locked!'
+                print 'The door is going up!'
+                while TopHall==1 and runTime<Door_Time:
+                        GPIO.output(35,True)
+                        GPIO.output(37,False)
+                        TopHall=GPIO.input(33)
+                        runTime=time.clock()-TimeStart
+                if 45==runTime:
+                        print 'Something went wrong, go check the door!'
+                        Safe_Kill()
+                if TopHall==0:
+                        print 'Door is open!'
+                        Safe_Kill()
+        elif TopHall==0 or Door_Action=='close': #Door is open
+                print 'The door is open!'
+                print 'The door is going down!'
+                while BottomHall==1 and runTime<Door_Time:
+                        GPIO.output(35,False)
+                        GPIO.output(37,True)
+                        BottomHall=GPIO.input(31)
+                        runTime=time.clock()-TimeStart
+                if 45==runTime:
+                        print 'Something went wrong, go check the door!'
+                        Safe_Kill()
+                if BottomHall==0:
+                        print 'Door is locked!'
+                        Safe_Kill()
+        runTime=time.clock()-TimeStart
+        print 'Total Time: '+str(runTime)
+DoorControl()
+Safe_Kill()
 
